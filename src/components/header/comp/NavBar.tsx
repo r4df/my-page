b2902f18f6267787df7c81af18cb633fcd5ccfbb
r4df <delafuente.rodolfo4@gmail.com>
2024-06-29 11:53:26 +0900

    062924_1

--- src/components/header/comp/NavBar.tsx
+++ src/components/header/comp/NavBar.tsx
@@ -1,61 +1,51 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
+// Assets
 import "../comp/NavBar.css";
-import MyLine from "../pages/common/MyLine.tsx";
-import Navbar from "react-bootstrap/Navbar";
-import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
-import Image from "react-bootstrap/Image";
-
 import SiteLogImg from "../comp/SiteLogo.jpeg";
 
 function NavBar() {
-  const [click, setClick] = useState(false);
+  const [stateNavLink, setNavLink] = useState("");
 
-  const handleClick = () => {
-    setClick(!click);
+  const toggleNavLink = () => {
+    setNavLink(stateNavLink === "" ? "active" : "");
   };
 
   return (
     <>
-      <Navbar expand="lg" className="bg-mycolor-1" sticky="top">
-        <Container>
-          <Navbar.Brand>
-            <Link to={"/"} className="nav-mybrand">
-              <Row className="d-flex align-items-center">
-                <Col>
-                  <Image
-                    src={SiteLogImg}
-                    className="img-mysitelogo"
-                    fluid
-                    roundedCircle
-                  ></Image>
-                </Col>
-                <Col>
-                  <h1 className="m-0 font-zxspect">
-                    R4DF
-                  </h1>
-                </Col>
-              </Row>
-            </Link>
-          </Navbar.Brand>
-          <Nav className="flex-row">
-            <Nav.Item className="mx-2">
-              <Link to={"/About"} className="btn btn-outline-mycolor-1">
-                About <i className="fa-solid fa-user-astronaut"></i>
+      <nav className="nav-bar sticky-top">
+        <div className="nav-title-menu">
+          <div className="mybrand-title">
+            <img src={SiteLogImg} alt="R4DF :)" />
+            <h1>R4DF</h1>
+          </div>
+
+          <button onClick={toggleNavLink} className="btn nav-menu">
+            <i className="fa-solid fa-bars"></i> Menu
+          </button>
+        </div>
+
+        <div className={`nav-links ${stateNavLink}`}>
+          <ul>
+            <li>
+              <Link to={"/"} className="btn">
+                <i className="fa-solid fa-house"></i> Home
               </Link>
-            </Nav.Item>
-            <Nav.Item className="mx-2">
-              <Link to={"/Contact"} className="btn btn-outline-mycolor-1">
-                Contact <i className="fa-solid fa-at"></i>
+            </li>
+            <li>
+              <Link to={"/About"} className="btn">
+                <i className="fa-solid fa-user-astronaut"></i> About
               </Link>
-            </Nav.Item>
-          </Nav>
-        </Container>
-      </Navbar>
+            </li>
+            <li>
+              <Link to={"/Contact"} className="btn">
+                <i className="fa-solid fa-envelope"></i> Contact
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </nav>
     </>
   );
 }
